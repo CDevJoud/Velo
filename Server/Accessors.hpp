9bc650a101b6953b9b9243195b1035de88ea9814
Joud Kandeel <joudkandeel05@gmail.com>
2026-06-03 22:16:06 +0200

    added a util for safe accessing ptr data to avoid using after free in system critical environments

---
+++ Server/Accessors.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace velo {
+	template<typename R, typename T, typename F>
+	R safe_access(const std::shared_ptr<T>& ptr, F&& fn, R fallback = R{}) {
+		if (ptr) {
+			return fn(ptr);
+		}
+		return fallback;
+	}
+
+	template<typename T, typename F>
+	inline void safe_access(const std::shared_ptr<T>& ptr, F&& fn) {
+		if (ptr) {
+			fn(ptr);
+		}
+	}
+}
